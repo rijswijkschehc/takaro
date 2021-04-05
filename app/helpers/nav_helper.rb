5a33e8baec66c71afdcd327e9c8ecf63db733ad8
Joost van Rijn <joost@joostvanrijn.nl>
2021-04-05 23:40:27 +0200

    Remove nav helper

--- app/helpers/nav_helper.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module NavHelper
-  def navbar_toggler(target:)
-    tag.button(class: 'navbar-toggler', type: :button, data: { bs: { toggle: 'collapse', target: target } }) do
-      tag.span(class: 'navbar-toggler-icon')
-    end
-  end
-end
