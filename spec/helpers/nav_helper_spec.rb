6ba512d05e85ca82e8cad18a8fb50437f68655ba
Joost van Rijn <joost@joostvanrijn.nl>
2021-04-07 18:12:47 +0200

    Remove nav helper spec

--- spec/helpers/nav_helper_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.describe NavHelper do
-  describe 'navbar_toggler' do
-    it 'renders the navbar toggler' do
-      expect(helper.navbar_toggler(target: '#foo')).to have_tag(:button)
-    end
-  end
-end
