df8e0115c803405284ae0119c8e6db55106ba179
Joost van Rijn <joost@joostvanrijn.nl>
2021-05-17 20:56:58 +0200

    Include Capybara matchers for decorator specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,6 +61,9 @@ RSpec.configure do |config|
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
 
+  # Include capybara matchers for custom decorator spec type
+  config.include Capybara::RSpecMatchers, type: :decorator
+
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
