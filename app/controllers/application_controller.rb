33201cdeeb5fb290570d918169cbe594ff7175ac
Joost van Rijn <joost@joostvanrijn.nl>
2021-06-02 17:59:11 +0200

    Add user info to Sentry

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,17 @@ class ApplicationController < ActionController::Base
   layout -> { user_signed_in? ? 'private' : 'public' }
 
   before_action :set_paper_trail_whodunnit
+  before_action :set_sentry_user
 
   def user_for_paper_trail
     current_user
+  end
+
+  private
+
+  def set_sentry_user
+    return if current_user.blank?
+
+    Sentry.set_user(id: current_user.id, email: current_user.email)
   end
 end
