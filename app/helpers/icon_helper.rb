e17500b41f656b778838daa020be2b3e9f600d00
Joost van Rijn <joost@joostvanrijn.nl>
2021-06-15 23:34:59 +0200

    Add margins to icons

--- app/helpers/icon_helper.rb
+++ app/helpers/icon_helper.rb
@@ -1,7 +1,16 @@
 # frozen_string_literal: true
 
 module IconHelper
-  def icon(id:)
-    tag.i(class: "fas fa-#{id}")
+  def icon(id:, margin: nil)
+    tag.i(class: classes(id: id, margin: margin))
+  end
+
+  private
+
+  def classes(id:, margin:)
+    %w[fas].tap do |a|
+      a << "fa-#{id}"
+      a << "m#{margin}"
+    end
   end
 end
