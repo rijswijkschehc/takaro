98d915dc91f886399b27c77fe86ef00b9e769e82
Joost van Rijn <joost@joostvanrijn.nl>
2021-06-15 19:09:35 +0200

    Log possession_phase_id for principles

--- app/models/principle.rb
+++ app/models/principle.rb
@@ -2,7 +2,7 @@
 
 class Principle < ApplicationRecord
   acts_as_list
-  has_paper_trail only: %i[description hex_color icon name position possession_phase tagline]
+  has_paper_trail only: %i[description hex_color icon name position possession_phase_id tagline]
   has_rich_text :description
 
   default_scope { order(:position) }
