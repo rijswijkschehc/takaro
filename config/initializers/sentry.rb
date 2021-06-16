44164a25c855065dfc534ed94097e5dfa99e590c
Joost van Rijn <joost@joostvanrijn.nl>
2021-06-16 23:03:56 +0200

    Update sentry logging
    - Send events in the background
    - Don't send events triggered from /admin/sidekiq

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,8 +1,21 @@
 # frozen_string_literal: true
 
 Sentry.init do |config|
+  config.async = lambda do |event, hint|
+    Sentry::SendEventJob.perform_later(event, hint)
+  end
+
   config.enabled_environments = %w[production]
   config.dsn = Credentials.fetch(:sentry, :dsn)
   config.breadcrumbs_logger = [:active_support_logger]
-  config.traces_sample_rate = 1.0
+  config.traces_sampler = lambda do |sampling_context|
+    next sampling_context[:parent_sampled] unless sampling_context[:parent_sampled].nil?
+
+    case sampling_context.dig(:transaction_context, :name)
+    when %r{admin/sidekiq}
+      0.0
+    else
+      1.0
+    end
+  end
 end
