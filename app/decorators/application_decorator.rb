1c060ed1a8421e30cc28ebb06119c1e178c157ad
Joost van Rijn <joost@joostvanrijn.nl>
2021-06-18 01:40:33 +0200

    Move email and created_at to designated decorator

--- app/decorators/application_decorator.rb
+++ app/decorators/application_decorator.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
 
 class ApplicationDecorator < Draper::Decorator
-  delegate :email, :created_at
 end
--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class UserDecorator < ApplicationDecorator
+  delegate :email, :created_at
+
   decorates_association :roles
 end
