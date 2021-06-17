27201290a1555f04e1354c29914d71ce5860f6dc
Joost van Rijn <joost@joostvanrijn.nl>
2021-06-18 01:34:57 +0200

    Move "name" out of the public decorator api

--- app/decorators/role_decorator.rb
+++ app/decorators/role_decorator.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 class RoleDecorator < ApplicationDecorator
-  delegate :name
-
   def badge
-    h.tag.span(name, class: 'badge bg-secondary')
+    h.tag.span(model.name, class: 'badge bg-secondary')
   end
 end
