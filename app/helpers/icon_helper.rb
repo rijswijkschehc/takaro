5bb9bf2a5c717c995beafe6f637006327a2165ef
Joost van Rijn <joost@joostvanrijn.nl>
2021-06-21 22:10:16 +0200

    Add fixed width option to icon helper

--- app/helpers/icon_helper.rb
+++ app/helpers/icon_helper.rb
@@ -1,16 +1,17 @@
 # frozen_string_literal: true
 
 module IconHelper
-  def icon(id:, margin: nil)
-    tag.i(class: classes(id: id, margin: margin))
+  def icon(id:, margin: nil, fixed: false)
+    tag.i(class: classes(id: id, margin: margin, fixed: fixed))
   end
 
   private
 
-  def classes(id:, margin:)
+  def classes(id:, margin:, fixed:)
     %w[fas].tap do |a|
       a << "fa-#{id}"
-      a << "m#{margin}"
+      a << "m#{margin}" if margin
+      a << 'fa-fw' if fixed
     end
   end
 end
