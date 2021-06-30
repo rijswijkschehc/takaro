b7a58dd9415aa0f69cedda054838b01c2fb7a28a
Joost van Rijn <joost@joostvanrijn.nl>
2021-06-30 21:51:10 +0200

    Add name to default mailer sender

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'noreply@rhcspeelwijze.nl'
+  default from: 'RHC Speelwijze <noreply@rhcspeelwijze.nl>'
   layout 'mailer'
 end
