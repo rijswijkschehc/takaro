3de61ba5dc27b074a31f23ed4334d411855650f4
Joost van Rijn <joost@joostvanrijn.nl>
2021-06-30 21:06:18 +0200

    Rename render_static_section

--- app/helpers/static_section_helper.rb
+++ app/helpers/static_section_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module StaticSectionHelper
-  def render_static_section(identifier:)
+  def static_section(identifier:)
     StaticSection.find_by!(identifier: identifier).content
   rescue ActiveRecord::RecordNotFound
     Sentry.capture_message("StaticSection with identifier: '#{identifier}' could not be found") and return
