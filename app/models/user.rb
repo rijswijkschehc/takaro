0405cb6f636587902de07a96721641521ccf54f0
Joost van Rijn <joost@joostvanrijn.nl>
2021-07-07 23:23:27 +0200

    Make registerable a todo

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@
 class User < ApplicationRecord
   include Roles
 
-  # :registerable is disabled for now: Will re-enable when actual registrations are open
+  # TODO: registerable is disabled for now: Will re-enable when actual registrations are open
   devise :database_authenticatable, :validatable, :rememberable,
          :recoverable, :trackable, :confirmable, :lockable
   has_paper_trail only: %i[email encrypted_password]
