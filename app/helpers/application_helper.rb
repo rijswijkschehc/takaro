3d8921ea2cbbeb90e34367a8eac6e765bac97bd9
Joost van Rijn <joost@joostvanrijn.nl>
2022-01-16 12:58:23 +0100

    Fix Rails 7 session issue

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  def session?
-    session.is_a?(ActionDispatch::Request::Session)
-  end
-
   def combined_dom_id(*records, prefix: nil)
     records.map do |record|
       record = record.model if record.decorated?
