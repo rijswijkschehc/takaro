f4dc7cc3ae61d52c526b35c7d0a17cae9bdb4b94
Joost van Rijn <joost@joostvanrijn.nl>
2022-05-26 13:05:14 +0200

    Fix has_many order
    To prevent a ActiveRecord::HasManyThroughOrderError

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,8 +3,8 @@
 class Team < ApplicationRecord
   has_paper_trail only: %i[name]
 
-  has_many :people, through: :team_members
   has_many :team_members, dependent: :destroy
+  has_many :people, through: :team_members
 
   validates :name, presence: true
 end
